b50c753503788da963efed6a4fd313225c357da1
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-10-30 14:36:53 +0000

    gcal set up

---
+++ gcal/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'gcal'
+
+urlpatterns = [
+    path('init/', views.google_calendar_init, name='google_calendar_init'),
+    path('redirect/', views.google_calendar_redirect, name='google_calendar_redirect'),
+]
---
+++ gcal/views.py
@@ -0,0 +1,75 @@
+import json
+from django.shortcuts import redirect, reverse
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+
+
+@login_required
+def google_calendar_init(request):
+    flow = Flow.from_client_secrets_file(
+        client_secrets_file=None,  # Use client config from settings
+        client_config={
+            "web": {
+                "client_id": settings.GOOGLE_OAUTH2_CLIENT_ID,
+                "client_secret": settings.GOOGLE_OAUTH2_CLIENT_SECRET,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "redirect_uris": [settings.GOOGLE_OAUTH2_REDIRECT_URI],
+            }
+        },
+        scopes=[
+            "https://www.googleapis.com/auth/calendar.events",
+            "https://www.googleapis.com/auth/calendar.readonly",
+        ],
+        redirect_uri=settings.GOOGLE_OAUTH2_REDIRECT_URI
+    )
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        prompt='consent'
+    )
+
+    request.session['oauth_state'] = state
+    return redirect(authorization_url)
+
+
+@login_required
+def google_calendar_redirect(request):
+    state = request.session.pop('oauth_state', '')
+
+    flow = Flow.from_client_secrets_file(
+        client_secrets_file=None,  # Use client config from settings
+        client_config={
+            "web": {
+                "client_id": settings.GOOGLE_OAUTH2_CLIENT_ID,
+                "client_secret": settings.GOOGLE_OAUTH2_CLIENT_SECRET,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "redirect_uris": [settings.GOOGLE_OAUTH2_REDIRECT_URI],
+            }
+        },
+        scopes=[
+            "https.www.googleapis.com/auth/calendar.events",
+            "https://www.googleapis.com/auth/calendar.readonly",
+        ],
+        redirect_uri=settings.GOOGLE_OAUTH2_REDIRECT_URI,
+        state=state
+    )
+
+    flow.fetch_token(authorization_response=request.build_absolute_uri())
+
+    credentials = flow.credentials
+    request.user.google_calendar_credentials = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
+    request.user.save()
+
+    return redirect(reverse('profile'))
