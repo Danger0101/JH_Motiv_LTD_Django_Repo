e6b0a816030d30632ba0c7e57cae5bffd101f109
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-11-30 15:50:02 +0000

    debug

--- coaching_availability/forms.py
+++ coaching_availability/forms.py
@@ -1,20 +1,39 @@
 from django import forms
-from .models import CoachAvailability, DateOverride, CoachVacation
-from django.forms import TimeInput
-from datetime import time, timedelta
+from django.forms import formset_factory
+from .models import CoachAvailability, DateOverride, CoachVacation, CoachAvailability
 
-class WeeklyScheduleForm(forms.ModelForm): # Changed from forms.Form to forms.ModelForm
+class WeeklyScheduleForm(forms.ModelForm):
     class Meta:
         model = CoachAvailability
         fields = ['day_of_week', 'start_time', 'end_time']
         widgets = {
             'start_time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
             'end_time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
-            # We hide day_of_week because we display it as text in the template
-            'day_of_week': forms.HiddenInput(), 
+            'day_of_week': forms.HiddenInput(),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Ensure the label doesn't show up for the hidden field
         self.fields['day_of_week'].label = ""
+
+class DateOverrideForm(forms.ModelForm):
+    class Meta:
+        model = DateOverride
+        fields = ['date', 'is_available', 'start_time', 'end_time']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'start_time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
+        }
+
+class CoachVacationForm(forms.ModelForm):
+    class Meta:
+        model = CoachVacation
+        fields = ['start_date', 'end_date', 'existing_booking_handling']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.TimeInput(attrs={'type': 'date'}),
+        }
+
+BaseWeeklyScheduleFormSet = formset_factory(WeeklyScheduleForm, extra=0)
+DAYS_OF_WEEK = CoachAvailability.DAYS_OF_WEEK
