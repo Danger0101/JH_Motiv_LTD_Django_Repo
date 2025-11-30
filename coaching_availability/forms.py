21db587daa382782340782926800f57d50f9e0db
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-11-30 15:06:05 +0000

    update

--- coaching_availability/forms.py
+++ coaching_availability/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import CoachAvailability, DateOverride, CoachVacation
 from django.forms import TimeInput
+from datetime import time, timedelta
 
 class CoachAvailabilityForm(forms.ModelForm):
     class Meta:
@@ -30,10 +31,18 @@ DAYS_OF_WEEK = [
     (6, 'Sunday'),
 ]
 
+def get_time_choices():
+    choices = [('', '-----')]
+    for hour in range(24):
+        for minute in range(0, 60, 15):
+            t = time(hour, minute)
+            choices.append((t.strftime('%H:%M:%S'), t.strftime('%I:%M %p')))
+    return choices
+
 class WeeklyScheduleForm(forms.Form):
     day_of_week = forms.ChoiceField(choices=DAYS_OF_WEEK, widget=forms.HiddenInput)
-    start_time = forms.TimeField(widget=TimeInput(attrs={'type': 'time', 'step': 900}), required=False)
-    end_time = forms.TimeField(widget=TimeInput(attrs={'type': 'time', 'step': 900}), required=False)
+    start_time = forms.TimeField(widget=forms.Select(choices=get_time_choices()), required=False)
+    end_time = forms.TimeField(widget=forms.Select(choices=get_time_choices()), required=False)
 
     def clean(self):
         cleaned_data = super().clean()
