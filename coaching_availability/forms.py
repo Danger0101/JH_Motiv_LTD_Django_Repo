682093ada1a842cd92545b3d859d6dbaec7cf1cb
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-11-30 13:53:32 +0000

    consolidating profile page and bringing back availability

--- coaching_availability/forms.py
+++ coaching_availability/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from .models import CoachAvailability, DAY_CHOICES
 
-class CoachAvailabilityForm(forms.Form):
-    pass
+class CoachAvailabilityForm(forms.ModelForm):
+    day_of_week = forms.MultipleChoiceField(
+        choices=DAY_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        label="Select Days"
+    )
 
-class CoachVacationForm(forms.Form):
-    pass
+    class Meta:
+        model = CoachAvailability
+        fields = ['day_of_week', 'start_time', 'end_time']
+        widgets = {
+            'start_time': forms.TimeInput(attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
