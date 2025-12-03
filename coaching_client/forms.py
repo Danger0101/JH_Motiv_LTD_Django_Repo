c0ccb63df5959ce92fc5c6e4a57fb721ec353254
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-03 18:48:17 +0000

    update

--- coaching_client/forms.py
+++ coaching_client/forms.py
@@ -1,19 +1,28 @@
 from django import forms
 from .models import TasterSessionRequest
 
+class GuestTasterRequestForm(forms.ModelForm):
+    """
+    Form for guest users to request a taster session.
+    """
+    class Meta:
+        model = TasterSessionRequest
+        fields = ['full_name', 'email', 'phone_number', 'goal_summary']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['goal_summary'].label = "Your Coaching Goals"
+
+
 class TasterRequestForm(forms.ModelForm):
     """
     Simplified form only requiring the goal summary from a logged-in user.
     """
     class Meta:
         model = TasterSessionRequest
-        # Only require goal_summary field in the form itself
         fields = ['goal_summary'] 
         
     def __init__(self, *args, **kwargs):
-        # Remove the custom 'request' kwarg used in the view for auto-population
         kwargs.pop('request', None) 
         super().__init__(*args, **kwargs)
-        
-        # NOTE: Bootstrap classes are now applied directly in the template partial
         self.fields['goal_summary'].label = "Your Coaching Goals"
