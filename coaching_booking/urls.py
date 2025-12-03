5a98fccd7252e81197fb9ce446a644bc0bcf9bfa
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-03 15:16:33 +0000

    fixed issues

--- coaching_booking/urls.py
+++ coaching_booking/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'coaching_booking'
 
 urlpatterns = [
-    path('', views.coach_landing_view, name='coach_landing'),
+    path('', views.CoachLandingView.as_view(), name='coach_landing'),
     path('offers/', views.OfferListView.as_view(), name='offer-list'),
     path('enroll/<slug:slug>/', views.OfferEnrollmentStartView.as_view(), name='offer-enroll'),
     
