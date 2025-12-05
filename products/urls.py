204bf89407740a94d0e748072d315705c507c90a
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-05 23:41:02 +0000

    debug

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductListView.as_as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetailView.as_as_view(), name='product_detail'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     
     # --- Printful Webhook Endpoint ---
     path('webhook/printful/', views.printful_webhook, name='printful_webhook'),
