b721ae559c59baeeb9d4cee59f9266def5efac58
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-18 11:21:30 +0000

    debug test

--- JH_Motiv_Shop/settings.py
+++ JH_Motiv_Shop/settings.py
@@ -18,7 +18,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jhmotiv-shop-ltd-official-040e4cbd5800.herokuapp.com', 'jhmotiv.shop', 'www.jhmotiv.shop']
