490de0d71be2166255dba638724c477411c823c7
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-28 16:12:50 +0000

    Update forms.py

--- dreamers/forms.py
+++ dreamers/forms.py
@@ -7,7 +7,7 @@ from .models import DreamerProfile
 class DreamerApplicationForm(forms.ModelForm):
     agree_terms = forms.BooleanField(
         required=True,
-        label=mark_safe('I agree to the <a href="/legal/dreamer-terms/" target="_blank" class="text-indigo-600 hover:text-indigo-500 underline">Dreamer Terms of Service</a>'),
+        label=mark_safe('I agree to the <a href="/terms-of-service/?tab=dreamer" target="_blank" class="text-indigo-600 hover:text-indigo-500 underline">Dreamer Terms of Service</a>'),
         widget=forms.CheckboxInput(attrs={'class': 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'}),
     )
     
