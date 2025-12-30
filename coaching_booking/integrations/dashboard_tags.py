22c8d07c68084e69eeee250daf86caaf4db151bb
John Hummel <34178990+Danger0101@users.noreply.github.com>
2025-12-30 10:22:33 +0000

    update

--- coaching_booking/integrations/dashboard_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from coaching_booking.models import CoachReview
-
-register = template.Library()
-
-@register.inclusion_tag('admin/dashboard_widgets/pending_reviews.html')
-def render_pending_reviews():
-    reviews = CoachReview.objects.filter(status='PENDING_STAFF').select_related('coach__user', 'client').order_by('-created_at')[:5]
-    return {'reviews': reviews}
