2d4cca580b5fbfe01c5cefb0fa901b4ae14450f9
John Hummel <34178990+Danger0101@users.noreply.github.com>
2026-02-16 22:05:58 +0000

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, CoachProfile, Address, MarketingPreference
+from unfold.admin import ModelAdmin
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(ModelAdmin, UserAdmin):
     actions = ['delete_selected']
     fieldsets = UserAdmin.fieldsets + (
         ('Roles', {'fields': ('is_guest', 'is_coach', 'is_client', 'is_dreamer')}),
@@ -14,18 +15,18 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('is_coach', 'is_client', 'is_guest', 'is_active')
 
 @admin.register(CoachProfile)
-class CoachProfileAdmin(admin.ModelAdmin):
+class CoachProfileAdmin(ModelAdmin):
     list_display = ('user', 'is_available_for_new_clients', 'average_dice_rating', 'review_count', 'last_synced')
     list_filter = ('is_available_for_new_clients',)
     search_fields = ('user__email', 'user__first_name', 'user__last_name')
     readonly_fields = ('average_dice_rating', 'review_count', 'last_synced')
 
 @admin.register(Address)
-class AddressAdmin(admin.ModelAdmin):
+class AddressAdmin(ModelAdmin):
     list_display = ('user', 'city', 'country', 'is_default')
     search_fields = ('user__email', 'street_address', 'postcode')
 
 @admin.register(MarketingPreference)
-class MarketingPreferenceAdmin(admin.ModelAdmin):
+class MarketingPreferenceAdmin(ModelAdmin):
     list_display = ('user', 'is_subscribed', 'updated_at')
     list_filter = ('is_subscribed',)
